b5a2f54ca792eff999d03cd3c11cb87c5b9ea6cf
Ben <rollin.ben@gmail.com>
2016-11-09 23:35:22 -0800

    Refactoring API code to allow multiple instances to exist concurrently, by utilizing MethodView.

--- src/flasky_thing.py
+++ src/flasky_thing.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, json
+from flask.views import MethodView
 from collections import OrderedDict
 import psycopg2
 import sqlite3
@@ -47,6 +48,7 @@ def main():
         # Give the headers in the HTTP response
         return display_text + '<p><p>Headers:<p>' + request.headers.__str__().replace('\n','<p>')
 
+    # TODO move this function inside class, add better comment
     def check_database_inputs(change_dict, table):
         
         # Ensure that all columns in change_dict are in the table definition
@@ -55,134 +57,108 @@ def main():
                 return 'Column %s not found in table!' % col
 
     # Class view_set creates a set of views for HTTP requests and routes them to a resource endpoint
-    class ViewSet:
+    class APIViewSet( MethodView ):
         
         # Constructor accepts options to determine the views to create
-        def __init__( self, server, connection, cursor, endpoint, table, views=['GET','PUT','DELETE','POST'] ):
+        def __init__( self, resource, connection, cursor, table ):
 
             # Store input parameter values into instance member variables
-            self.server     = server
             self.connection = connection
             self.cursor     = cursor
-            self.endpoint   = endpoint
+            self.resource   = resource
             self.table      = table
-            self.views      = views
-
-            # Resource name is the last item in the relative endpoint path
-            self.resource   = self.endpoint.split('/')[-1]
 
             # Primary key is the first column of the table
             self.pri_key    = self.table.keys()[0]
             
-            assert len(self.resource) > 2, "The specified endpoint must be in the form: /path/from/host/to/resource"
+        # GET method will return the JSON for the resource with that ID
+        def get(self, id):
 
-            self.create_views()
+            if id is None:
+                return 'Not implemented yet!'
+                # TODO return list of all resources
 
-        # Create views as specified for this instance
-        def create_views( self ):
-            
-            if 'GET' in self.views:
-                
-                # GET method will return the JSON for the resource with that ID
-                @self.server.route(self.endpoint + '/<int:id>', methods = ['GET'])
-                def get_resource(id):
-                    
-                    try:
-                        self.cursor.execute('SELECT * FROM ' + self.resource + ' WHERE ' + \
-                            self.pri_key + ' = %s', (str(id),) )
-                        record = self.cursor.fetchone()
+            else:
+                try:
+                    self.cursor.execute('SELECT * FROM ' + self.resource + ' WHERE ' + \
+                        self.pri_key + ' = %s', (str(id),) )
+                    record = self.cursor.fetchone()
 
-                        if not record:
-                            return 'Unsuccessful. No resource with ' + self.pri_key + ' %d found.' % id, 404
+                    if not record:
+                        return 'Unsuccessful. No resource with ' + self.pri_key + ' %d found.' % id, 404
 
-                        return jsonify( OrderedDict( zip( self.table.keys(), record ) ) )
+                    return jsonify( OrderedDict( zip( self.table.keys(), record ) ) )
 
-                    except Exception as e:
-                        return 'Unsuccessful. Error:\n' + str(e), 400
+                except Exception as e:
+                    return 'Unsuccessful. Error:\n' + str(e), 400
 
-                self.get = get_resource
+        # PUT method will update the record with the supplied JSON info
+        def put(self, id):
 
-            elif 'PUT' in self.views:
+            try:
+                # Capture HTTP request data (JSON) and load into an update dictionary
+                update_dict = OrderedDict( json.loads( request.data ) )
 
-                # PUT method will update the record with the supplied JSON info
-                @self.server.route(self.endpoint + '/<int:id>', methods = ['PUT'])
-                def put_resource(id):
+                # Ensure that if the id exists in update_dict, it matches the id provided in URL
+                if self.pri_key in update_dict:
+                    if id != int( update_dict[self.pri_key] ):
+                        return 'Error: ID does not match in body and URL', 400
+                # If the id is not in update_dict, add it
+                else:
+                    update_dict['id'] = id
 
-                    try:
-                        # Capture HTTP request data (JSON) and load into an update dictionary
-                        update_dict = OrderedDict( json.loads( request.data ) )
+                # Perform checks on dict describing values to be updated
+                inputs_check = check_database_inputs( update_dict, self.table )
+                if inputs_check:
+                    return 'Error: ' + inputs_check, 400
 
-                        # Ensure that if the id exists in update_dict, it matches the id provided in URL
-                        if self.pri_key in update_dict:
-                            if id != int( update_dict[self.pri_key] ):
-                                return 'Error: ID does not match in body and URL', 400
-                        # If the id is not in update_dict, add it
-                        else:
-                            update_dict['id'] = id
+                # Execute and commit SQL command
+                sql = 'UPDATE ' + self.resource + '\nSET\n' + ',\n'.join([x + ' = %(' + x + ')s' for x in update_dict.keys()]) + '\nWHERE ' + self.pri_key + ' = %(id)s;'
+                self.cursor.execute( sql, update_dict )
+                self.connection.commit()
 
-                        # Perform checks on dict describing values to be updated
-                        inputs_check = check_database_inputs( update_dict, self.table )
-                        if inputs_check:
-                            return 'Error: ' + inputs_check, 400
+                return 'Successfully updated resource with following SQL command:\n' + sql % update_dict, 201
 
-                        # Execute and commit SQL command
-                        sql = 'UPDATE ' + self.resource + '\nSET\n' + ',\n'.join([x + ' = %(' + x + ')s' for x in update_dict.keys()]) + '\nWHERE ' + self.pri_key + ' = %(id)s;'
-                        self.cursor.execute( sql, update_dict )
-                        self.connection.commit()
+            except Exception as e:
+                return 'Unsuccessful. Error:\n' + str(e), 400
 
-                        return 'Successfully updated resource with following SQL command:\n' + sql % update_dict, 201
+        # DELETE method will delete the record with the matching id
+        def delete(self, id):
 
-                    except Exception as e:
-                        return 'Unsuccessful. Error:\n' + str(e), 400
+            # Execute and commit SQL command
+            sql = 'DELETE FROM ' + self.resource + ' WHERE id = %s'
+            self.cursor.execute(sql, {self.pri_key:id} )
+            self.connection.commit()
 
-                self.put = put_resource
+            return 'Successfully deleted resource with ' + self.pri_key + ' %d' % id, 201
 
-            elif 'DELETE' in self.views:
+        # POST method will create a record with the supplied JSON info
+        def post(self):
 
-                # DELETE method will delete the record with the matching id
-                @self.server.route(self.endpoint + '/<int:id>', methods = ['DELETE'])
-                def delete_resource(id):
+            try:
+                # Capture HTTP request data in JSON
+                insert_dict = OrderedDict( json.loads( request.data ) )
 
-                    # Execute and commit SQL command
-                    sql = 'DELETE FROM ' + self.resource + ' WHERE id = %s'
-                    self.cursor.execute(sql, {self.pri_key:id} )
-                    self.connection.commit()
+                # Perform checks on dict describing values to be inserted
+                inputs_check = check_database_inputs( insert_dict, self.table )
+                if inputs_check:
+                    return 'Error: ' + inputs_check
 
-                    return 'Successfully deleted resource with ' + self.pri_key + ' %d' % id, 201
+                # Execute and commit SQL command
+                sql ='INSERT INTO ' + self.resource + '\n(' + ', '.join(insert_dict.keys()) + ')\nVALUES\n(' + \
+                    ', '.join( ['%(' + x + ')s' for x in insert_dict.keys()] )  + ');'
+                self.cursor.execute( sql, insert_dict )
+                self.connection.commit()
 
-                self.delete = delete_resource
+                return 'Successfully created resource with following SQL command:\n' + sql % insert_dict, 201
 
-            elif 'POST' in self.views:
+            except Exception as e:
+                return 'Unsuccessful. Error:\n' + str(e)
 
-                # POST method will create a record with the supplied JSON info
-                @self.server.route(self.endpoint, methods = ['POST'])
-                def post_resource(id):
-
-                    try:
-                        # Capture HTTP request data in JSON
-                        insert_dict = OrderedDict( json.loads( request.data ) )
-
-                        # Perform checks on dict describing values to be inserted
-                        inputs_check = check_database_inputs( insert_dict, self.table )
-                        if inputs_check:
-                            return 'Error: ' + inputs_check
-
-                        # Execute and commit SQL command
-                        sql ='INSERT INTO ' + self.resource + '\n(' + ', '.join(insert_dict.keys()) + ')\nVALUES\n(' + \
-                            ', '.join( ['%(' + x + ')s' for x in insert_dict.keys()] )  + ');'
-                        self.cursor.execute( sql, insert_dict )
-                        self.connection.commit()
-
-                        return 'Successfully created resource with following SQL command:\n' + sql % insert_dict, 201
-
-                    except Exception as e:
-                        return 'Unsuccessful. Error:\n' + str(e)
-
-                self.post = post_resource
-
-    # Test of views creator
-
-    members_views = ViewSet( server, pgconn, pgcurs, '/bentest/api/v1/members', members_table )
+    members_views = APIViewSet.as_view( 'members_api', 'members', pgconn, pgcurs, members_table )
+    server.add_url_rule( '/bentest/api/v1/members/', defaults={ 'id':None }, view_func=members_views, methods=['GET'] )
+    server.add_url_rule( '/bentest/api/v1/members/', view_func=members_views, methods=['POST'] )
+    server.add_url_rule( '/bentest/api/v1/members/<int:id>', view_func=members_views, methods=['GET','PUT','DELETE'] )
 
 #    @server.route('/bentest/api/v1/members/<int:id>', methods = ['GET','PUT','DELETE'])
 #    def members(id):
