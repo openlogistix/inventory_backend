45e4b1e0dd127165373faa61569277544d7042fa
Ben <rollin.ben@gmail.com>
2017-02-21 21:29:17 -0800

    Made secretfire_app subclass Flask.

--- src/secretfire_app.py
+++ src/secretfire_app.py
@@ -10,61 +10,67 @@ from flask.views import MethodView
 
 from views_api import create_api
 
-def create_pgconn():
+class SecretFireAPI(Flask):
 
-    # Initiate a connection to the postgres database
-    return psycopg2.connect('dbname=postgres user=postgres')
+    def __init__(self, name):
+        # Set up Flask server
+        Flask.__init__(self, name)
 
-def create_pgcurs(conn):
+        # Set up connection to postgres database
+        pgconn = self.create_pgconn()
+        pgcurs = self.create_pgcurs(pgconn)
 
-    # Create cursor in postgres database
-    return conn.cursor()
+        # Define members table column names and data types
+        members_cols        = ('id','name','phone_no','email','years','playa_name','location','notes')
+        members_cols_types  = ('int','text','text','text','int','text','text','text')
+        members_table       = OrderedDict( zip( members_cols, members_cols_types ) )
 
-def main():
+        # Create API for members resource
+        create_api( self, '/bentest/api/v1/members/',  members_table,  pgconn, pgcurs )
 
-    # Set up Flask server
-    server = Flask('secret_fire' )
+        # Define projects table column names and data types
+        projects_cols       = ('id','lead','description','budget')
+        projects_cols_types = ('int','int','text','int')
+        projects_table      = OrderedDict( zip( projects_cols, projects_cols_types ) )
 
-    # Set up connection to postgres database
-    pgconn = create_pgconn()
-    pgcurs = create_pgcurs(pgconn)
+        # Create API for projects resource
+        create_api( self, '/bentest/api/v1/projects/', projects_table, pgconn, pgcurs )
 
-    # Define members table column names and data types
-    members_cols        = ('id','name','phone_no','email','years','playa_name','location','notes')
-    members_cols_types  = ('int','text','text','text','int','text','text','text')
-    members_table       = OrderedDict( zip( members_cols, members_cols_types ) )
+        # Define project_membership table column names and data types
+        proj_memb_cols       = ('id','project_id','member_id')
+        proj_memb_cols_types = ('int','int','int')
+        proj_memb_table      = OrderedDict( zip( proj_memb_cols, proj_memb_cols_types ) )
 
-    # Create API for members resource
-    create_api( server, '/bentest/api/v1/members/',  members_table,  pgconn, pgcurs )
+        # Create API for project_membership resource
+        create_api( self, '/bentest/api/v1/project_membership/', proj_memb_table, pgconn, pgcurs )
 
-    # Define projects table column names and data types
-    projects_cols       = ('id','lead','description','budget')
-    projects_cols_types = ('int','int','text','int')
-    projects_table      = OrderedDict( zip( projects_cols, projects_cols_types ) )
+        @self.route('/bentest')
+        def home():
 
-    # Create API for projects resource
-    create_api( server, '/bentest/api/v1/projects/', projects_table, pgconn, pgcurs )
+            # Test response which gives headers
+            # return '<title>Testing</title>hello?! world?!..<p><p>Headers:<p>' + \
+            #   request.headers.__str__().replace('\n','<p>')
 
-    # Define project_membership table column names and data types
-    proj_memb_cols       = ('id','project_id','member_id')
-    proj_memb_cols_types = ('int','int','int')
-    proj_memb_table      = OrderedDict( zip( proj_memb_cols, proj_memb_cols_types ) )
+            return send_from_directory('static','index.html')
 
-    # Create API for project_membership resource
-    create_api( server, '/bentest/api/v1/project_membership/', proj_memb_table, pgconn, pgcurs )
 
-    @server.route('/bentest')
-    def home():
+    def create_pgconn(self):
 
-        # Test response which gives headers
-        # return '<title>Testing</title>hello?! world?!..<p><p>Headers:<p>' + \
-        #   request.headers.__str__().replace('\n','<p>')
-        print 'requesting index'
+        # Initiate a connection to the postgres database
+        return psycopg2.connect('dbname=postgres user=postgres')
 
-        return send_from_directory('static','index.html')
+    def create_pgcurs(self, conn):
 
-    # Run the server on port 7000
-    server.run('0.0.0.0',port=7000, debug=True)
+        # Create cursor in postgres database
+        return conn.cursor()
 
-main()
+
+config = {  'host':'0.0.0.0',
+            'port':7000,
+            'debug':True }
+
+if __name__ == '__main__':
+    app = SecretFireAPI(__name__)
+    app.run(**config)
+
 
