3703688c9277690af783b07ae4698a16e934dab7
brollin <rollin.ben@gmail.com>
2017-02-22 16:48:41 -0800

    Adding utility to create QR code images.

---
+++ util/composite.py
@@ -0,0 +1,18 @@
+import PIL.Image as Image
+import qrcode
+import sys
+
+sticker = Image.open("mockup.png")
+offset = (41, 406)
+
+n = int( sys.argv[1])
+for i in xrange(n):
+	qr = qrcode.QRCode(
+		border=0,
+	)
+	qr.add_data('http://se.cretfi.re/gear/{i}'.format(i=i))
+	qr_img = qr.make_image()
+	qr_img.thumbnail((210,210))
+	sticker.paste(qr_img, offset)
+
+	sticker.save("output/{i:03d}.png".format(i=i))
