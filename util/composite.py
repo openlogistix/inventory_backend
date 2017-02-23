60f749724c0ea7381a2cd2b7e2673be2a4da4c05
kevin <kevin@rainas-mbp.gateway.sonic.net>
2017-02-23 12:49:54 -0800

    Use a border, slightly smaller thumbnail

--- util/composite.py
+++ util/composite.py
@@ -8,11 +8,12 @@ offset = (41, 406)
 n = int( sys.argv[1])
 for i in xrange(n):
     qr = qrcode.QRCode(
-        border=0,
+        border=1,
     )
+    print "Generating image {i}...".format(i=i)
     qr.add_data('http://se.cretfi.re/gear/{i}'.format(i=i))
     qr_img = qr.make_image()
-    qr_img.thumbnail((210,210))
+    qr_img.thumbnail((196,196))
     sticker.paste(qr_img, offset)
 
     sticker.save("output/{i:03d}.png".format(i=i))
