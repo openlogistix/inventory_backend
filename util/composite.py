8df81f2929161eef48dcc5d56c3f7c3124593e65
brollin <rollin.ben@gmail.com>
2017-02-23 04:07:57 -0800

    Changed hard tabs to expanded spaces.

--- util/composite.py
+++ util/composite.py
@@ -7,12 +7,12 @@ offset = (41, 406)
 
 n = int( sys.argv[1])
 for i in xrange(n):
-	qr = qrcode.QRCode(
-		border=0,
-	)
-	qr.add_data('http://se.cretfi.re/gear/{i}'.format(i=i))
-	qr_img = qr.make_image()
-	qr_img.thumbnail((210,210))
-	sticker.paste(qr_img, offset)
+    qr = qrcode.QRCode(
+        border=0,
+    )
+    qr.add_data('http://se.cretfi.re/gear/{i}'.format(i=i))
+    qr_img = qr.make_image()
+    qr_img.thumbnail((210,210))
+    sticker.paste(qr_img, offset)
 
-	sticker.save("output/{i:03d}.png".format(i=i))
+    sticker.save("output/{i:03d}.png".format(i=i))
