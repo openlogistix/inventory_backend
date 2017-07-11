a86f97fb60637383f20b0fd757d973c5844c4ff9
Jeff Wang <jeffawang@gmail.com>
2017-07-10 22:50:48 -0700

    defaults: use port 1870 (legacy)

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,5 @@
 DEBUG = True
+SERVER_NAME = 'localhost:1870'
 STATICFILEPATH = '/var/www/openlogistix/attachments'
 SQLALCHEMY_DATABASE_URI = 'postgresql://openlogistix@localhost:5432/openlogistix'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
